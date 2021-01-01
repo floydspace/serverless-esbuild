c6fdcdf9fec2ce60b38f98b39aa6963752f62e57
subash <me@subash.com.au>
2021-01-02 01:46:13 +1100

    docs(watch-example): add example configuration for watch

--- examples/complete/src/index.ts
+++ examples/complete/src/index.ts
@@ -2,6 +2,7 @@ import validateIsin from 'isin-validator';
 
 export async function handler(event: string) {
   const isInvalid = validateIsin(event);
+  console.log('modified');
 
   return {
     statusCode: 200,
