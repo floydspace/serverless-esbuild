26bcff5c72667db723c90117665ddff634351e43
Olup <loup.topalian@gmail.com>
2021-03-02 17:01:11 +0100

    fix(offline): fix pre-offline function

--- src/pre-offline.ts
+++ src/pre-offline.ts
@@ -1,11 +1,14 @@
-import { EsbuildPlugin } from '.';
 import { relative } from 'path';
+import { assocPath } from 'ramda';
+import { EsbuildPlugin } from '.';
+
 export function preOffline(this: EsbuildPlugin) {
   // Set offline location automatically if not set manually
   if (!this.serverless?.service?.custom?.['serverless-offline']?.location) {
-    this.serverless.service.custom['serverless-offline'].location = relative(
-      this.serverless.config.servicePath,
-      this.buildDirPath
+    assocPath(
+      ['service', 'custom', 'serverless-offline', 'location'],
+      relative(this.serverless.config.servicePath, this.buildDirPath),
+      this.serverless
     );
   }
 }
