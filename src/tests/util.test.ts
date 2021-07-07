b76e29fcf920d9c646226587090537c084995949
Tony Tyrrell <tony@adept.at>
2021-07-07 11:56:31 -0700

    fix(utils): modify findProjectRoot to look for all yarn or package files in a single iteration

--- src/tests/util.test.ts
+++ src/tests/util.test.ts
@@ -1,11 +1,12 @@
 import { findProjectRoot } from '../utils';
 import path from 'path';
 
-describe('A Util Function.', () => {
-    it('Should properly Find a Project Root.', () => {
+describe('utils/findProjectRoot', () => {
+    it('should properly Find a Project Root.', () => {
+        /* Broken implementation in pack-externals we're trying to fix. */
         const rootPackageJsonPath = path.join(findProjectRoot(), './package.json');
-        console.log(rootPackageJsonPath);
 
+        /* Looking up at project root relative to ./src/tests/ */
         expect(rootPackageJsonPath).toEqual(path.join(__dirname, '../../package.json'));
     });
 });
