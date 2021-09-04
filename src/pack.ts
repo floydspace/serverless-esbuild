67cfe7dcc7345c0efe90289e7bdcbed9e01ce059
olup <olup@users.noreply.github.com>
2021-09-04 18:30:57 +0200

    fix(package): fix the archiving of individual files (#159)
    * fix(package): fix the archiving of individual files
    
    * refactor: fix fs copy usage
    
    Co-authored-by: Victor Korzunin <ifloydrose@gmail.com>

--- src/pack.ts
+++ src/pack.ts
@@ -75,7 +75,7 @@ export async function pack(this: EsbuildServerlessPlugin) {
     )(files);
 
     const startZip = Date.now();
-    await zip(artifactPath, filesPathList, this.buildDirPath);
+    await zip(artifactPath, filesPathList);
     const { size } = fs.statSync(artifactPath);
 
     this.serverless.cli.log(
@@ -157,7 +157,7 @@ export async function pack(this: EsbuildServerlessPlugin) {
         }));
 
       const startZip = Date.now();
-      await zip(artifactPath, filesPathList, this.buildDirPath);
+      await zip(artifactPath, filesPathList);
 
       const { size } = fs.statSync(artifactPath);
 
