f67c5d4c99b6ea6439943dd114daae8f369e9338
Jiří Špác <capajj@gmail.com>
2021-11-11 12:03:13 +0100

    fix: filtering in pack.ts
    fixes #195

---
+++ src/tests/pack.test.ts
@@ -0,0 +1,37 @@
+import { filterFilesForZipPackage } from '../pack';
+
+describe('filterFilesForZipPackage', () => {
+  it('should filter out files for another zip package', () => {
+    expect(
+      filterFilesForZipPackage({
+        files: [
+          {
+            localPath:
+              '__only_service-otherFnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h',
+            rootPath:
+              '/home/capaj/repos/google/search/.esbuild/.build/__only_service-otherFnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h',
+          },
+          {
+            localPath:
+              '__only_service-fnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h',
+            rootPath:
+              '/home/capaj/repos/google/search/.esbuild/.build/__only_service-fnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h',
+          },
+        ],
+        depWhiteList: [],
+        fnNameWithoutStage: 'service-fnName',
+        isGoogleProvider: false,
+        hasExternals: false,
+        includedFiles: [],
+        excludedFiles: [],
+      })
+    ).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "localPath": "__only_service-fnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h",
+          "rootPath": "/home/capaj/repos/google/search/.esbuild/.build/__only_service-fnName/bin/imagemagick/include/ImageMagick/magick/method-attribute.h",
+        },
+      ]
+    `);
+  });
+});
