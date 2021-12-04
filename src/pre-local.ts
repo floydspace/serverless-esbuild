2bfb9d0ac6dfdee437f0275ad9745bcc72116dba
Sam Chung <samc@seek.com.au>
2021-12-04 11:57:49 +1100

    fix: correct local invoke for non node functions

--- src/pre-local.ts
+++ src/pre-local.ts
@@ -1,7 +1,10 @@
 import { EsbuildServerlessPlugin } from '.';
+
 export function preLocal(this: EsbuildServerlessPlugin) {
   this.serviceDirPath = this.buildDirPath;
   this.serverless.config.servicePath = this.buildDirPath;
-  // Set service path as CWD to allow accessing bundled files correctly
-  process.chdir(this.serviceDirPath);
+  // If this is a node function set the service path as CWD to allow accessing bundled files correctly
+  if (this.functions[this.options.function]) {
+    process.chdir(this.serviceDirPath);
+  }
 }
