54c8f6b4f0c135e95f520087dcf1f1d644c2ed3f
Vamsi D <vamsi.akirah@gmail.com>
2021-12-15 19:31:26 +0530

    chore(npm.test.ts): refactoring

--- src/tests/packagers/npm.test.ts
+++ src/tests/packagers/npm.test.ts
@@ -3,75 +3,58 @@ import * as path from 'path';
 import * as mockSpawn from 'mock-spawn';
 
 describe('test NPM class', () => {
+  let spawnSpy = mockSpawn();
+
+  beforeEach(() => {
+    spawnSpy = mockSpawn();
+    require('child_process').spawn = spawnSpy;
+  });
+
   it('should properly get the dependencies list', async () => {
-    jest.resetAllMocks();
-
-    const mySpawn = mockSpawn();
-    require('child_process').spawn = mySpawn;
-
-    mySpawn.sequence.add(mySpawn.simple(0, '6.0.0'));
-    mySpawn.sequence.add(mySpawn.simple(0, '{"dependencies":{}}'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '6.0.0'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '{"dependencies":{}}'));
 
     const npm = new NPM();
     const dependencies = await npm.getProdDependencies(path.join('./'));
     
-    expect(mySpawn.calls.length).toStrictEqual(2);
-    expect(mySpawn.calls[0].args).toStrictEqual(['--version']);
-    expect(mySpawn.calls[1].args).toStrictEqual(['ls', '-json', '-prod']);
+    expect(spawnSpy.calls.length).toStrictEqual(2);
+    expect(spawnSpy.calls[0].args).toStrictEqual(['--version']);
+    expect(spawnSpy.calls[1].args).toStrictEqual(['ls', '-json', '-prod']);
     expect(dependencies).toStrictEqual({'dependencies':{}});
-    jest.resetAllMocks();
   });
 
   it('should properly get the dependencies list w/ depth', async () => {
-    jest.resetAllMocks();
-
-    const mySpawn = mockSpawn();
-    require('child_process').spawn = mySpawn;
-
-    mySpawn.sequence.add(mySpawn.simple(0, '{"dependencies":{}}'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '{"dependencies":{}}'));
 
     const npm = new NPM();
     const dependencies = await npm.getProdDependencies(path.join('./'), 2);
     
-    expect(mySpawn.calls.length).toStrictEqual(1);
-    expect(mySpawn.calls[0].args).toStrictEqual(['ls', '-json', '-prod', '-depth=2']);
+    expect(spawnSpy.calls.length).toStrictEqual(1);
+    expect(spawnSpy.calls[0].args).toStrictEqual(['ls', '-json', '-prod', '-depth=2']);
     expect(dependencies).toStrictEqual({'dependencies':{}});
-    jest.resetAllMocks();
   });
 
   it('should properly get the dependencies list (npm version >= 7)', async () => {
-    jest.resetAllMocks();
-
-    const mySpawn = mockSpawn();
-    require('child_process').spawn = mySpawn;
-
-    mySpawn.sequence.add(mySpawn.simple(0, '7.0.0'));
-    mySpawn.sequence.add(mySpawn.simple(0, '{"dependencies":{}}'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '7.0.0'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '{"dependencies":{}}'));
 
     const npm = new NPM();
     const dependencies = await npm.getProdDependencies(path.join('./'));
     
-    expect(mySpawn.calls.length).toStrictEqual(2);
-    expect(mySpawn.calls[0].args).toStrictEqual(['--version']);
-    expect(mySpawn.calls[1].args).toStrictEqual(['ls', '-json', '-prod', '-all']);
+    expect(spawnSpy.calls.length).toStrictEqual(2);
+    expect(spawnSpy.calls[0].args).toStrictEqual(['--version']);
+    expect(spawnSpy.calls[1].args).toStrictEqual(['ls', '-json', '-prod', '-all']);
     expect(dependencies).toStrictEqual({'dependencies':{}});
-    jest.resetAllMocks();
   });
 
   it('should properly get the dependencies list w/ depth (npm version >= 7)', async () => {
-    jest.resetAllMocks();
-
-    const mySpawn = mockSpawn();
-    require('child_process').spawn = mySpawn;
-
-    mySpawn.sequence.add(mySpawn.simple(0, '{"dependencies":{}}'));
+    spawnSpy.sequence.add(spawnSpy.simple(0, '{"dependencies":{}}'));
 
     const npm = new NPM();
     const dependencies = await npm.getProdDependencies(path.join('./'), 2);
     
-    expect(mySpawn.calls.length).toStrictEqual(1);
-    expect(mySpawn.calls[0].args).toStrictEqual(['ls', '-json', '-prod', '-depth=2']);
+    expect(spawnSpy.calls.length).toStrictEqual(1);
+    expect(spawnSpy.calls[0].args).toStrictEqual(['ls', '-json', '-prod', '-depth=2']);
     expect(dependencies).toStrictEqual({'dependencies':{}});
-    jest.resetAllMocks();
   });
 });
