ed8c019b5f7f494fcf2b5f052f76a501ab8ce9b3
Misha Bruml <mishabruml@gmail.com>
2022-03-12 20:44:28 +0000

    feat: first pass at e2e testing

---
+++ e2e/minimal.test.ts
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+test('minimal', () => {
+  const indexContents = fs.readFileSync('e2e-minimal/.serverless/index.js').toString();
+  expect(indexContents).toMatchSnapshot();
+});
