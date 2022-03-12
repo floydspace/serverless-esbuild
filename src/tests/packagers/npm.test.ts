7e4bc4d5f8055dd6ed0ad3f0f7b2804b05c88d95
Misha Bruml <mishabruml@gmail.com>
2022-03-12 19:29:55 +0000

    fix: remove erroneously committed test

--- src/tests/packagers/npm.test.ts
+++ src/tests/packagers/npm.test.ts
@@ -1004,14 +1004,6 @@ describe('NPM Packager', () => {
     expect(v7dependencies).toStrictEqual(expectedResult);
   });
 
-  test.each([
-    { a: 1, b: 1, expected: 2 },
-    { a: 1, b: 2, expected: 3 },
-    { a: 2, b: 1, expected: 3 },
-  ])('.add($a, $b)', ({ a, b, expected }) => {
-    expect(a + b).toBe(expected);
-  });
-
   it.each([
     'npm ERR! code ELSPROBLEMS',
     'npm ERR! extraneous: foo@1.2.3 ./bar/node_modules/foo',
