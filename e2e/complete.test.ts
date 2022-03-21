e15a4f7947d4c6b46b09f9917cebb96528341dda
Misha Bruml <mishabruml@gmail.com>
2022-03-21 13:45:58 +0000

    feat: target e2e test at local changes

--- e2e/complete.test.ts
+++ e2e/complete.test.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
 test('complete', () => {
-  const indexContents = fs.readFileSync('e2e-complete/.serverless/src/index.js').toString();
+  const indexContents = fs.readFileSync('e2e/complete/.serverless/src/index.js').toString();
   expect(indexContents).toMatchSnapshot();
-  const nodeModules = fs.readdirSync('e2e-complete/.serverless/node_modules').toString();
+  const nodeModules = fs.readdirSync('e2e/complete/.serverless/node_modules').toString();
   expect(nodeModules).toMatchSnapshot();
 });
--- e2e/individually.test.ts
+++ e2e/individually.test.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
 test('individually', () => {
-  const hello1indexContents = fs.readFileSync('e2e-individually/.serverless/hello1.js').toString();
-  const hello2indexContents = fs.readFileSync('e2e-individually/.serverless/hello2.js').toString();
+  const hello1indexContents = fs.readFileSync('e2e/individually/.serverless/hello1.js').toString();
+  const hello2indexContents = fs.readFileSync('e2e/individually/.serverless/hello2.js').toString();
   expect(hello1indexContents).toMatchSnapshot();
   expect(hello2indexContents).toMatchSnapshot();
 });
