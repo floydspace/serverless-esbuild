71e6534465261435c97c3abe4ccb9cd19f9eca8d
Misha Bruml <mishabruml@gmail.com>
2022-03-21 14:23:17 +0000

    fix: more robust e2e assertion

--- e2e/complete.test.ts
+++ e2e/complete.test.ts
@@ -4,5 +4,5 @@ test('complete', () => {
   const indexContents = fs.readFileSync('e2e/complete/.serverless/src/index.js').toString();
   expect(indexContents).toMatchSnapshot();
   const nodeModules = fs.readdirSync('e2e/complete/.serverless/node_modules').toString();
-  expect(nodeModules).toMatchSnapshot();
+  expect(nodeModules).toEqual(expect.stringContaining('isin-validator'));
 });
