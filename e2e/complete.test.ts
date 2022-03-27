dab0047e80ed21fc797acb0fa92e61fbed12808e
Misha Bruml <mishabruml@gmail.com>
2022-03-27 00:25:13 +0000

    feat: e2e testing, snapshot cloudformation

--- e2e/complete.test.ts
+++ e2e/complete.test.ts
@@ -1,8 +1,31 @@
 import fs from 'fs';
+import cloudformation from './complete/.serverless/cloudformation-template-update-stack.json';
 
 test('complete', () => {
   const indexContents = fs.readFileSync('e2e/complete/.serverless/src/index.js').toString();
   expect(indexContents).toMatchSnapshot();
+
   const nodeModules = fs.readdirSync('e2e/complete/.serverless/node_modules').toString();
   expect(nodeModules).toEqual(expect.stringContaining('isin-validator'));
+
+  expect(cloudformation.AWSTemplateFormatVersion).toMatchSnapshot();
+  expect(cloudformation.Description).toMatchSnapshot;
+  expect(cloudformation.Outputs).toMatchSnapshot();
+
+  const apiGatewayDeploymentPropertyKey = Object.keys(cloudformation.Resources).find((s) =>
+    s.startsWith('ApiGatewayDeployment')
+  ) as keyof typeof cloudformation.Resources;
+
+  const { [apiGatewayDeploymentPropertyKey]: apiGatewayDeployment, ...deterministicResources } =
+    cloudformation.Resources;
+
+  expect(deterministicResources).toMatchSnapshot({
+    ValidateIsinLambdaFunction: {
+      Properties: {
+        Code: { S3Key: expect.stringContaining('complete-example.zip') },
+      },
+    },
+  });
+
+  expect(apiGatewayDeployment).toMatchSnapshot();
 });
--- e2e/modules.d.ts
+++ e2e/modules.d.ts
@@ -2,3 +2,13 @@ declare module '*/minimal/.serverless/cloudformation-template-update-stack.json'
   const json: Record<string, unknown>;
   export default json;
 }
+
+declare module '*/individually/.serverless/cloudformation-template-update-stack.json' {
+  const json: Record<string, unknown>;
+  export default json;
+}
+
+declare module '*/complete/.serverless/cloudformation-template-update-stack.json' {
+  const json: Record<string, unknown>;
+  export default json;
+}
