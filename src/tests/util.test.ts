80bb08a864cbdc3b434d718945af799bb003da09
Victor Korzunin <5180700+floydspace@users.noreply.github.com>
2022-10-03 09:00:17 +0200

    refactor: improve utils with more functional way

--- src/tests/util.test.ts
+++ src/tests/util.test.ts
@@ -1,5 +1,11 @@
+import fs from 'fs-extra';
+import mockFs from 'mock-fs';
 import path from 'path';
-import { findProjectRoot } from '../utils';
+import extract from 'extract-zip';
+import globby from 'globby';
+import crypto from 'crypto';
+
+import { findProjectRoot, zip } from '../utils';
 
 describe('utils/findProjectRoot', () => {
   it('should properly Find a Project Root.', () => {
@@ -9,4 +15,68 @@ describe('utils/findProjectRoot', () => {
     /* Looking up at project root relative to ./src/tests/ */
     expect(rootPackageJsonPath).toEqual(path.join(__dirname, '../../package.json'));
   });
+});
+
+describe('utils/zip', () => {
+  beforeEach(() => {
+    mockFs({
+      '/src': {
+        'test.txt': 'lorem ipsum',
+      },
+      '/dist': {},
+    });
+  });
+
+  afterEach(() => {
+    mockFs.restore();
+  });
+
+  it('should fail with.', async () => {
+    const source = '/src';
+    const destination = '/dist';
+    const zipPath = path.join(destination, 'archive.zip');
+    const filesPathList = [
+      {
+        rootPath: path.join(source, 'incorrect.txt'),
+        localPath: 'test.txt',
+      },
+    ];
+
+    await expect(zip(zipPath, filesPathList)).rejects.toThrowError(
+      // eslint-disable-next-line @typescript-eslint/quotes
+      "ENOENT, no such file or directory '/src/incorrect.txt'"
+    );
+  });
+
+  it.each([{ useNativeZip: true }, { useNativeZip: false }])(
+    'should properly archive files.',
+    async ({ useNativeZip }) => {
+      const source = '/src';
+      const destination = '/dist';
+      const zipPath = path.join(destination, 'archive.zip');
+      const filesPathList = [
+        {
+          rootPath: path.join(source, 'test.txt'),
+          localPath: 'test.txt',
+        },
+      ];
+
+      await zip(zipPath, filesPathList, useNativeZip);
+
+      expect(fs.existsSync(zipPath)).toEqual(true);
+
+      await extract(zipPath, { dir: destination });
+
+      const files = await globby(['**/*'], { cwd: destination });
+
+      expect(files).toEqual(['archive.zip', 'test.txt']);
+
+      // native zip is not deterministic
+      if (!useNativeZip) {
+        const data = fs.readFileSync(zipPath);
+        const fileHash = crypto.createHash('sha256').update(data).digest('base64');
+        expect(fileHash).toEqual('tfgMhOD8tXzhzap6rJLQJbUXK2lawANGsUcWRgNED4M=');
+      }
+    }
+  );
 });
