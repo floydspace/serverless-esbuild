17cc8e6365f82accc541fcfc59cd2096edd862c2
Bruno Negrão <bnegrao@gmail.com>
2024-10-12 11:50:41 -0300

    Revert "fix: preserve mtime when zipping with the node zipper (#539)"
    This reverts commit 162d9bc7f0e87d63cd93adb0f4756d01769da79d.

--- src/utils.ts
+++ src/utils.ts
@@ -112,7 +112,7 @@ function nodeZip(zipPath: string, filesPathList: IFiles): Promise<void> {
       zipArchive.append(fs.readFileSync(file.rootPath), {
         name: file.localPath,
         mode: stats.mode,
-        date: new Date(stats.mtime),
+        date: new Date(0), // necessary to get the same hash when zipping the same content
       });
     });
 
